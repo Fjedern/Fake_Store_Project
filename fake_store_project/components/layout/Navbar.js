b1a3d31e77ed3ee2753c2fed00b05af197856e5f
David <david@nlsn.nu>
2021-10-19 14:19:51 +0200

    Navbar and homepage getting data from api

--- fake_store_project/components/layout/Navbar.js
+++ fake_store_project/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Navbar() {
+export default function Navbar({categories}) {
   return (
     <nav
       style={{
@@ -11,8 +11,20 @@ export default function Navbar() {
       }}
     >
       <Link href="/">Hem</Link>
+      {console.log(categories)}
+      {/* {categories.map((category) => (
+        <Link key={category} href="">{category}</Link>
+      ))} */}
       <Link href="about">About us</Link>
       <Link href="cart">Cart</Link>
     </nav>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://fakestoreapi.com/products/categories");
+  const data = await res.json();
+
+  console.log(data);
+  return { props: { categories: data } };
 }
