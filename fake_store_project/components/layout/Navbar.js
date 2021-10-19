292a6f9bd491f6668021182897f587bcf7531525
PalmquistS <s.palmquist@telia.com>
2021-10-19 15:42:36 +0200

    cleanup in navbar

--- fake_store_project/components/layout/Navbar.js
+++ fake_store_project/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "../../styles/NavAndFooter.module.css"
 
-export default function Navbar({categories}) {
+export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Link href="/">Hem</Link>
