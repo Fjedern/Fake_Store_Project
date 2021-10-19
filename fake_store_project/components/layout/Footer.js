1f25d6519d24975f2a8f3c6a20e1d9713f5e3776
David <david@nlsn.nu>
2021-10-19 16:10:54 +0200

    Simple layout fix

--- fake_store_project/components/layout/Footer.js
+++ fake_store_project/components/layout/Footer.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import styles from "../../styles/NavAndFooter.module.css"
+import styles from "../../styles/NavAndFooter.module.css";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <Link href="/">Home</Link>
-      <Link href="about">About us</Link>
-      <Link href="cart">Cart</Link>
+      <div className={styles.footerLinks}>
+        <Link href="/">Home</Link>
+        <Link href="about">About us</Link>
+        <Link href="cart">Cart</Link>
+      </div>
+      <div className={styles.footerCreators}>
+        <p>Fjedern</p>
+        <p>PalquistS</p>
+        <p>Swala</p>
+      </div>
     </div>
   );
 }
