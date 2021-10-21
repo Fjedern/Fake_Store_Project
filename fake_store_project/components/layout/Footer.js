26e3cda1dde6cc35da4f0ce6769eb17049266810
Sandra Wahlgren <71629445+Swala@users.noreply.github.com>
2021-10-21 15:31:08 +0200

    footer styling and about.js

--- fake_store_project/components/layout/Footer.js
+++ fake_store_project/components/layout/Footer.js
@@ -3,17 +3,48 @@ import styles from "../../styles/NavAndFooter.module.css";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footerLinks}>
-        <Link href="/">Home</Link>
-        <Link href="about">About us</Link>
-        <Link href="cart">Cart</Link>
-      </div>
-      <div className={styles.footerCreators}>
-        <p>Fjedern</p>
-        <p>PalquistS</p>
-        <p>Swala</p>
-      </div>
-    </div>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
+      />
+      <footer className="bg-gray-800 text-white body-font">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-gray-500 text-sm text-center sm:text-left">
+            © 2021 Copyright:{" "}
+            <p className="inline-flex text-gray-500 ml-1">
+              Fjedern Swala PalmquistS
+            </p>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <div className="flex space-x-4">
+              <Link href="/about" className="text-gray-400" passHref>
+                <a className="hover:bg-gray-700 px-3 py-2 rounded-md ml-1">
+                  {" "}
+                  About Us
+                </a>
+              </Link>
+              <Link href="/" className="text-gray-400" passHref>
+                <a className="hover:bg-gray-700 px-3 py-2 rounded-md ml-1">
+                  Sign Up
+                </a>
+              </Link>
+            </div>
+          </span>
+
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a href="" className="ml-3 text-gray-500">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="" className="ml-3 text-gray-500">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a href="" className="ml-3 text-gray-500">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </span>
+        </div>
+      </footer>
+    </>
   );
 }
